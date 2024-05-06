3a78a9f9cefd43f547968a66fbec351648c2899a
Emmy <machadodeoliveirairigoitaemmy@gmail.com>
2024-05-05 23:05:03 -0300

    Navbar terminado

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,22 +1,31 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import { NavLink } from 'react-router-dom';
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { NavLink } from "react-router-dom";
 
 import { CartWidget } from "./CartWidget";
 
 export const NavBar = () => {
-    return (
+  return (
     <Navbar bg="light" data-bs-theme="light">
-        <Container>
-            <Navbar.Brand to="/App.jsx">Emmline</Navbar.Brand>
-            <Nav className="me-auto">
-            <Nav.Link to="/category/ropa" as={NavLink}>Ropa</Nav.Link>
-            <Nav.Link to="/category/accesorios" as={NavLink}>Accesorios</Nav.Link>
-            <Nav.Link to="/category/albums" as={NavLink}>Albums</Nav.Link>
+      <Container>
+        <Navbar.Brand to="/">Emmline</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link to="/category/ropa" as={NavLink}>
+            Ropa
+          </Nav.Link>
+          <Nav.Link to="/category/accesorios" as={NavLink}>
+            Accesorios
+          </Nav.Link>
+          <Nav.Link to="/category/albums" as={NavLink}>
+            Albums
+          </Nav.Link>
+          <Nav.Link to="/category/lightstick" as={NavLink}>
+            Lightsticks
+          </Nav.Link>
         </Nav>
         <CartWidget />
-        </Container>
+      </Container>
     </Navbar>
-    );
+  );
 };
